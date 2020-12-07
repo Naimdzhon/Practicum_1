1a2e1a2e9fa4dd0122dbaa60171923d46b56578e
Naimdzhon <63001628+Naimdzhon@users.noreply.github.com>
2020-12-07 23:12:57 +0300

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,97 @@
+#include "Problem.h"
+#include <iostream>
+
+int main() {
+  {
+    std::string s = "ab+c.aba..bac.+.+";
+    int k = 3;
+    int l = 1;
+    std::cout << "TEST_1" << '\n'
+              << "Input:" << '\t' << "String: " << s << '\t' << "Divider: " << k
+              << '\t' << "Remainder: " << l << '\n';
+    std::cout << "Output:" << '\t';
+    Get_Minimal_String(s, k, l);
+  }
+  {
+    std::string s = "acb..bab.c.*.ab.ba.+.+*a.";
+    int k = 3;
+    int l = 0;
+    std::cout << "TEST_2" << '\n'
+              << "Input:" << '\t' << "String: " << s << '\t' << "Divider: " << k
+              << '\t' << "Remainder: " << l << '\n';
+    std::cout << "Output:" << '\t';
+    Get_Minimal_String(s, k, l);
+  }
+  {
+    std::string s = "ab+c.aba..bac.+.+";
+    int k = 3;
+    int l = 2;
+    std::cout << "TEST_3" << '\n'
+              << "Input:" << '\t' << "String: " << s << '\t' << "Divider: " << k
+              << '\t' << "Remainder: " << l << '\n';
+    std::cout << "Output:" << '\t';
+    Get_Minimal_String(s, k, l);
+  }
+  {
+    std::string s = "abcb...ab.+*";
+    int k = 4;
+    int l = 3;
+    std::cout << "TEST_4" << '\n'
+              << "Input:" << '\t' << "String: " << s << '\t' << "Divider: " << k
+              << '\t' << "Remainder: " << l << '\n';
+    std::cout << "Output:" << '\t';
+    Get_Minimal_String(s, k, l);
+  }
+  {
+    std::string s = "accb...1+";
+    int k = 4;
+    int l = 0;
+    std::cout << "TEST_5" << '\n'
+              << "Input:" << '\t' << "String: " << s << '\t' << "Divider: " << k
+              << '\t' << "Remainder: " << l << '\n';
+    std::cout << "Output:" << '\t';
+    Get_Minimal_String(s, k, l);
+  }
+  {
+    std::string s = "abcb...aab..+*";
+    int k = 6;
+    int l = 5;
+    std::cout << "TEST_6" << '\n'
+              << "Input:" << '\t' << "String: " << s << '\t' << "Divider: " << k
+              << '\t' << "Remainder: " << l << '\n';
+    std::cout << "Output:" << '\t';
+    Get_Minimal_String(s, k, l);
+  }
+  {
+    std::string s = "abc*.+bac.+.";
+    int k = 1;
+    int l = 0;
+    std::cout << "TEST_7" << '\n'
+              << "Input:" << '\t' << "String: " << s << '\t' << "Divider: " << k
+              << '\t' << "Remainder: " << l << '\n';
+    std::cout << "Output:" << '\t';
+    Get_Minimal_String(s, k, l);
+  }
+  {
+    std::string s = "acb..bab.c.*.ab.ba.+.+*a";
+    int k = 4;
+    int l = 3;
+    std::cout << "TEST_8" << '\n'
+              << "Input:" << '\t' << "String: " << s << '\t' << "Divider: " << k
+              << '\t' << "Remainder: " << l << '\n';
+    std::cout << "Output:" << '\t';
+    Get_Minimal_String(s, k, l);
+  }
+  {
+    std::string s = "aba.*.a.*ab1+..";
+    int k = 5;
+    int l = 4;
+    std::cout << "TEST_9" << '\n'
+              << "Input:" << '\t' << "String: " << s << '\t' << "Divider: " << k
+              << '\t' << "Remainder: " << l << '\n';
+    std::cout << "Output:" << '\t';
+    Get_Minimal_String(s, k, l);
+  }
+
+  return 0;
+}
